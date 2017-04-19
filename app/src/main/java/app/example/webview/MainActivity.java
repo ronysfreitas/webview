9ce6e01cb5ac63e9706c12e73a8b14fc31d5ea26
Rony Freitas <ronyfreitas98@gmail.com>
2017-04-19 15:34:14 -0300

    Set Up WebView

--- app/src/main/java/app/example/webview/MainActivity.java
+++ app/src/main/java/app/example/webview/MainActivity.java
@@ -1,13 +1,138 @@
 package app.example.webview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // WebView and ProgressBar
+        webView = (WebView) findViewById(R.id.webview);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+
+        // Set Up WebView
+        setUpWebView();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void setUpWebView() {
+        WebSettings webSettings = webView.getSettings();
+
+        // Enabling JavaScript
+        webSettings.setJavaScriptEnabled(true);
+
+        // Gets whether the WebView supports zoom
+        webSettings.setSupportZoom(false);
+
+        //  Settings to enable/disable cache in WebView
+        webView.setDrawingCacheEnabled(true);
+
+        // Settings to disable text selection in WebView
+        webView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+
+        // Settings to enable Cookie in WebView
+        CookieManager cookieManager = CookieManager.getInstance();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            CookieSyncManager.createInstance(this);
+        }
+        cookieManager.setAcceptCookie(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (Uri.parse(url).getHost().equals("www.example.com")) {
+                    // This is my web site, so do not override; let my WebView load the page
+                    return false;
+                }
+                // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                // Page Started
+                webView.setVisibility(View.INVISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                // Page Finished
+                progressBar.setVisibility(View.INVISIBLE);
+                webView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                // In case of error
+            }
+        });
+
+        // Use remote resource
+        webView.loadUrl("http://www.example.com");
+
+        // Use local resource
+        // mWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    // Function for reload WebView
+    private void webViewReload() {
+        webView.reload();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.reload) {
+            webViewReload();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
